8dd61b5450ddabc79f38d6e8409d796cc47702bd
Naru <jefferson.arlines@gmail.com>
2026-06-07 11:09:40 -0300

    Add files via upload

---
+++ repetidorcodigo.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <string>
+int contador;
+bool COISO;
+int main(){
+    do
+    {
+     std::cout <<  contador;
+    contador = contador + 1;
+    if (contador == 1000)
+    {
+    std::cout << "eita poooooooooooooooooooooooooooooooooooooxaaaaaaaaaaaaaaaaaaaaaaaaaaaaa";
+    }
+    if(contador == 1254)
+    {
+        std::cout << "pc hackeado";
+    }
+ 
+    
+
+    } while (COISO == true);
+
+    
+
+}
